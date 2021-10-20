e1540ab002bf58bb3054cd6d8b34ed5d2023a3b4
Jeremy Clark <jeremy@jeremybytes.com>
2021-10-20 15:21:02 -0700

    loading file and parsing records

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use std::fs;
+use std::io;
+
+#[derive(Debug)]
+pub struct Record {
+    pub actual: u8,
+    pub image: [u8; 784],
+}
+
+pub fn get_data(filename: String) -> io::Result<Vec<Record>> {
+    let mut results = Vec::new();
+    let contents = get_raw_data(filename);
+    for line in contents {
+        let parsed = parse_raw_data(&line).clone();
+        let rec = parse_record(parsed);
+        results.push(rec);
+    }
+    Ok(results)
+}
+
+pub fn get_raw_data(filename: String) -> Vec<String> {
+    let contents = fs::read_to_string(filename).unwrap();
+    let splits: Vec<&str> = contents.split("\r\n").skip(1).collect();
+    let mut results = Vec::new();
+    for split in splits {
+        results.push(split.clone().to_string())
+    }
+    results
+}
+
+pub fn parse_raw_data(raw_data: &str) -> Vec<u8> {
+    let mut results = Vec::new();
+    let items: Vec<&str> = raw_data.split(',').collect();
+    for item in items
+    {
+        let i: u8 = match item.trim().parse() {
+            Ok(value) => value,
+            Err(_) => continue,
+        };
+        results.push(i);
+    }
+    results
+}
+
+pub fn parse_record(data: Vec<u8>) -> Record {
+    let mut iterator = data.into_iter();
+    let actual = iterator.next().unwrap_or_default();
+    let mut values: [u8; 784] = [0; 784];
+    let mut index = 0;
+    for i in iterator {
+        values[index] = i;
+        index += 1;
+    }
+    Record {
+        actual,
+        image: values,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let contents = digits::get_data("train.csv".to_string()).unwrap();
+    let mut count = 0;
+    for line in contents {
+        if count > 10 {
+            break;
+        }
+        println!("{:?}", line);
+        count += 1;
+    }
+    
 }
