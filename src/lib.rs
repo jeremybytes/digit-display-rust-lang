0faa62da8a3cc0561da3519e688887fb11ea8cda
Jeremy Clark <jeremy@jeremybytes.com>
2021-10-23 23:45:45 -0700

    working on modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::fs;
 use std::io;
 
 #[derive(Debug)]
@@ -6,6 +5,8 @@ pub struct Record {
     pub actual: u8,
     pub image: [u8; 784],
 }
+
+use self::loading::*;
 
 pub fn get_data(filename: String) -> io::Result<Vec<Record>> {
     let mut results = Vec::new();
@@ -18,41 +19,45 @@ pub fn get_data(filename: String) -> io::Result<Vec<Record>> {
     Ok(results)
 }
 
-pub fn get_raw_data(filename: String) -> Vec<String> {
-    let contents = fs::read_to_string(filename).unwrap();
-    let splits: Vec<&str> = contents.split("\r\n").skip(1).collect();
-    let mut results = Vec::new();
-    for split in splits {
-        results.push(split.clone().to_string())
+pub mod loading {
+    use std::fs;
+    
+    pub fn get_raw_data(filename: String) -> Vec<String> {
+        let contents = fs::read_to_string(filename).unwrap();
+        let splits: Vec<&str> = contents.split("\r\n").skip(1).collect();
+        let mut results = Vec::new();
+        for split in splits {
+            results.push(split.clone().to_string())
+        }
+        results
     }
-    results
-}
 
-pub fn parse_raw_data(raw_data: &str) -> Vec<u8> {
-    let mut results = Vec::new();
-    let items: Vec<&str> = raw_data.split(',').collect();
-    for item in items
-    {
-        let i: u8 = match item.trim().parse() {
-            Ok(value) => value,
-            Err(_) => continue,
-        };
-        results.push(i);
+    pub fn parse_raw_data(raw_data: &str) -> Vec<u8> {
+        let mut results = Vec::new();
+        let items: Vec<&str> = raw_data.split(',').collect();
+        for item in items
+        {
+            let i: u8 = match item.trim().parse() {
+                Ok(value) => value,
+                Err(_) => continue,
+            };
+            results.push(i);
+        }
+        results
     }
-    results
-}
 
-pub fn parse_record(data: Vec<u8>) -> Record {
-    let mut iterator = data.into_iter();
-    let actual = iterator.next().unwrap_or_default();
-    let mut image: [u8; 784] = [0; 784];
-    let mut index = 0;
-    for i in iterator {
-        image[index] = i;
-        index += 1;
-    }
-    Record {
-        actual,
-        image,
+    pub fn parse_record(data: Vec<u8>) -> super::Record {
+        let mut iterator = data.into_iter();
+        let actual = iterator.next().unwrap_or_default();
+        let mut image: [u8; 784] = [0; 784];
+        let mut index = 0;
+        for i in iterator {
+            image[index] = i;
+            index += 1;
+        }
+        super::Record {
+            actual,
+            image,
+        }
     }
 }
