2c82ef73790ae1e70f223b6a9f673892bf08eca1
Jeremy Clark <jeremy@jeremybytes.com>
2021-10-30 11:35:09 -0700

    Added chunking (throttle number of threads)

--- src/lib.rs
+++ src/lib.rs
@@ -15,30 +15,33 @@ pub struct Record {
 }
 
 pub fn run(config: configuration::Config) {
-    let (training, validation) = get_data("train.csv".to_string(), config.offset, config.count).unwrap();
+    let (training, validation) = loader::get_data("train.csv".to_string(), config.offset, config.count).unwrap();
     println!("Data load complete");
 
     let start = Instant::now();
     let mut errors = Vec::new();
 
     let (tx, rx) = mpsc::channel();
-    for line in validation {
+
+    let chunks = loader::chunk_data(validation, 6);
+
+    for chunk in chunks {
         let classifier = match &config.classifier[..] {
             "Manhattan Classifier" => recognize::get_manhattan_classifier(training.clone()),
             "Euclidean Classifier" => recognize::get_euclidean_classifier(training.clone()),
             _ => recognize::get_euclidean_classifier(training.clone()),
         };
-    
         let tx = tx.clone();
         thread::spawn(move || {
-            let (actual, predicted) = classifier.predict(&line);
-            tx.send((actual, predicted)).unwrap();
-        });
+            for line in chunk {
+                let (actual, predicted) = classifier.predict(&line);
+                tx.send((actual, predicted)).unwrap();
+        }});
     }
 
     for _ in 0..config.count {
         let (actual, predicted) = rx.recv().unwrap();
-        display_images(&actual, &predicted);
+        display_images(&actual, &predicted, false);
 
         if predicted.actual != actual.actual {
             errors.push((actual, predicted));
@@ -57,7 +60,7 @@ pub fn run(config: configuration::Config) {
     println!("{}", "=".repeat(56));
 
     for (actual, predicted) in errors {
-        display_images(&actual, &predicted);
+        display_images(&actual, &predicted, true);
     }
 
     print_summary(&config.classifier, config.offset, config.count, elapsed_time, total_errors);
@@ -76,35 +79,19 @@ pub fn print_summary(
     println!("Total errors: {}", total_errors);
 }
 
-pub fn get_data(filename: String, offset: usize, count: usize) -> io::Result<(Vec<Record>, Vec<Record>)> {
-    let mut results = Vec::new();
-    let contents = loader::get_raw_data(filename);
-    for line in contents {
-        let parsed = loader::parse_raw_data(&line);
-        let rec = match loader::parse_record(parsed) {
-            Ok(val) => val,
-            Err(message) => {
-                eprintln!("Bad record: {}", message);
-                continue;
-            },
-        };
-        results.push(rec);
+pub fn display_image(data: &Record, scroll: bool) {
+    if !scroll {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     }
-    let data_sets = loader::split_data_sets(results, offset, count);
-
-    Ok(data_sets)
-}
-
-pub fn display_image(data: &Record) {
     let image = display::get_image_as_string(data.image);
-    print!("{}", image);
-    println!("{}", "=".repeat(56));
+    print!("{}/n{}", image, "=".repeat(56));
 }
 
-pub fn display_images(actual: &Record, predicted: &Record) {
-    println!("Actual: {} {} | Predicted: {}", actual.actual, " ".repeat(46), predicted.actual);
+pub fn display_images(actual: &Record, predicted: &Record, scroll: bool) {
+    if !scroll {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    }
     let image = display::get_images_as_string(actual.image, predicted.image);
-    print!("{}", image);
-    println!("{}", "=".repeat(115));
+    println!("Actual: {} {} | Predicted: {}\n{}\n{}", actual.actual, " ".repeat(46), predicted.actual, image, "=".repeat(115));
 }
 
